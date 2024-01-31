12e3b35ec197d2dd1b3254519aaad9f66a07d3cd
tyubraj96 <131554500+tyubraj96@users.noreply.github.com>
2024-01-31 22:18:16 +0545

    add project files

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+(function () {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+  let heroCarouselIndicators = select("#hero-carousel-indicators");
+  let heroCarouselItems = select("#heroCarousel .carousel-item", true);
+
+  heroCarouselItems.forEach((item, index) => {
+    index === 0
+      ? (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "' class='active'></li>")
+      : (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" +
+          index +
+          "'></li>");
+  });
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const gallaryLightbox = GLightbox({
+    selector: "gallary-lightbox",
+  });
+  const Lightbox = GLightbox({
+    selector: ".glightbox",
+  });
+
+  /**
+   * Portfolio details slider
+   */
+
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+  /**
+    /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+})(jQuery);
